f5df1184af85f8eba3e84ec9628d02ed67bc12f1
DanielM14 <37811239+DanielM14@users.noreply.github.com>
2019-11-01 15:11:53 -0500

    update debugging

---
+++ DebugOne2/DebugOne2/Program.cs
@@ -0,0 +1,16 @@
+﻿// Filename DebugOne2.cs
+// This program outputs a large C#
+using static System.Console;
+class DebugOne2
+{
+    static void main()
+    {
+        WriteLine("CCCCC   #  #");
+
+        WriteLine("C     ########");
+      WriteLine("C       #  #");
+  
+        WriteLine("C     ########");
+        WriteLine("CCCCC   #  #");
+    }
+}
---
+++ DebugOne3/DebugOne3/Program.cs
@@ -0,0 +1,15 @@
+﻿// Filename DebugOne3.cs
+// This program outputs directions to a party
+using System;
+class DebugOne3
+{
+    static void Main()
+    {
+        Console.WriteLine("Take Highway 51 north");
+        Console.WriteLine("Exit at County Road H");
+        Console.WriteLine("Go three blocks and make a right on School Street");
+        Console.WriteLine("Go right at Elm");
+        Console.WriteLine("Party is four houses down on the left");
+    }
+
+}
